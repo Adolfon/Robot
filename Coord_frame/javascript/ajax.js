1267a4972b78fd1004a201b04126fc1da6637c80
Adolfon <holahola>
2016-03-24 17:20:07 +0900

    production version

--- Coord_frame/javascript/ajax.js
+++ Coord_frame/javascript/ajax.js
@@ -1,16 +1,51 @@
+//WebSocket version ====================================
 
+// Include a reference to closeSocket() and openSocket() functions 
+//inside of the script related to submenu click processing (settingMenu.js). 
+function openSocket(coord_system){    	
+        // Ensures only one connection is open at a time       	
+        if(webSocket !== undefined && webSocket.readyState !== WebSocket.CLOSED)           
+            return;
+        
+        // Create a new instance of the websocket
+        webSocket = new WebSocket("ws://localhost/coordWS");
+                       
+        //when I have a message from the server...       
+        webSocket.onmessage = function(msg){
+        	
+	            webSocket.send(coord_system);        		
+        		var resp= JSON.parse(msg);
+	            writeResponse(resp,coord_system);	            
+        };                       
+    }
+ 
+    function closeSocket(){
+        webSocket.close();
+    }
 
-//Jquery version
+    function writeResponse(text,coord){
+        if (coord='XY'){
+        	$("#ax1_value").text(text.AX1);
+            $("#ax2_value").text(text.AX2);
+            $("#ax3_value").text(text.AX3);
+            $("#ax4_value").text(text.AX4);
+            $("#ax5_value").text(text.AX5);
+            $("#ax6_value").text(text.AX6);
+        }
+    }
+
+//Jquery version ====================================
 
 $(document).ready(function () {		
 	    var interval = 50;   // ms per call   
 	    var refresh = function() {
-	        $.ajax({
-	            url: "http://localhost:8080/robot/RobotResponse",
+	        $.ajax({	            
+	        	url: "http://192.168.240.69/cgi-bin/get_current_position.py",
 	            cache: false,	            
 	            crossDomain:false,	            
-	            success: function(result) {	            		            	
-	                $("#coord").html("X: "+result.X+",Y: "+result.Y+",Z: "+result.Z+",S: "+result.S);                
+	            success: function(result) {	            		            		                                
+	            	$("#coord").html("AX1: "+result.AX1+",AX2: "+result.AX2+",AX3: "+result.AX3+",AX4: "
+	            			+result.AX4+",AX5: "+result.AX5+",AX6: "+result.AX6+",system: "+result.system);
 	                setInterval(refresh, interval);
 	            }
 	        });
@@ -19,7 +54,7 @@ $(document).ready(function () {
 	});
 
 
-// Javascript version 
+// Javascript version  =================================
 
 function ajax(){	
 	var xhttp;
@@ -39,7 +74,7 @@ function ajax(){
 }
 
 function refresh(){	
-	setInterval("ajax();",50);
+	setInterval("ajax();",550);
 }
 
 
